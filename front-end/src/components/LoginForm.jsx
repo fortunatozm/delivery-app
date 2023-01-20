6f4209360241ee9219084c4fada3859f5756e265
samuel5g <samuelmonteiro21@gmail.com>
2023-01-20 17:56:18 -0300

    feat(component): :sparkles: Adds login component

---
+++ front-end/src/components/LoginForm.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+function LoginForm() {
+  return (
+    <form>
+      <label htmlFor="email">
+        Login
+        <input
+          type="email"
+          name="email"
+          id="email"
+          data-testid="common_login__input-email"
+        />
+      </label>
+
+      <label htmlFor="senha">
+        Senha
+        <input
+          type="password"
+          name="password"
+          id="password"
+          data-testid="common_login__input-password"
+        />
+      </label>
+
+      <button
+        type="button"
+        data-testid="common_login__button-login"
+      >
+        Login
+
+      </button>
+      <button
+        type="button"
+        data-testid="common_login__button-register"
+      >
+        Ainda não tenho conta
+
+      </button>
+    </form>
+  );
+}
+
+export default LoginForm;
