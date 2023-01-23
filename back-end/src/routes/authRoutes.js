5289b82a7e90c5476a0d73303c94b09d9e89b5da
samuel5g <samuelmonteiro21@gmail.com>
2023-01-23 18:11:52 -0300

    fix(log): :rotating_light: Remove console.log

--- back-end/src/routes/authRoutes.js
+++ back-end/src/routes/authRoutes.js
@@ -2,7 +2,6 @@ const { Router } = require('express');
 const { loginController, registerController } = require('../controllers');
 
 const authRoutes = Router();
-console.log('authRoutes');
 
 authRoutes.post('/login', loginController.loginControllerPost);
 authRoutes.post('/register', registerController.registerControllerPost);
