879e700028c1f85f0c625a3d04555d948bb3378a
samuel5g <samuelmonteiro21@gmail.com>
2023-01-23 18:06:17 -0300

    feat(login): :sparkles: Add form validation in email and password fields

--- front-end/src/components/LoginForm.jsx
+++ front-end/src/components/LoginForm.jsx
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function LoginForm() {
+  const [formData, setFormData] = useState({ email: '', password: '' });
+  const [submitButtonDisabled, setSubmitButtonDisabled] = useState(true);
+
+  const validateFormData = () => {
+    const MIN_PASSWORD_SIZE = 6;
+
+    const isValidEmail = /\S+@\S+\.\S+/.test(formData.email);
+    const isValidPassword = formData.password.length >= MIN_PASSWORD_SIZE;
+
+    setSubmitButtonDisabled(!(isValidEmail && isValidPassword));
+  };
+
+  useEffect(validateFormData, [formData]);
+
+  const handleChange = ({ target: { name, value } }) => {
+    setFormData((prevState) => ({ ...prevState, [name]: value }));
+  };
+
   return (
     <form>
       <label htmlFor="email">
         Login
         <input
+          onChange={ handleChange }
+          value={ setFormData.email }
           type="email"
           name="email"
           id="email"
@@ -16,6 +36,8 @@ function LoginForm() {
       <label htmlFor="senha">
         Senha
         <input
+          onChange={ handleChange }
+          value={ setFormData.password }
           type="password"
           name="password"
           id="password"
@@ -24,6 +46,7 @@ function LoginForm() {
       </label>
 
       <button
+        disabled={ submitButtonDisabled }
         type="button"
         data-testid="common_login__button-login"
       >
