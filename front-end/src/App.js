dc8be2006f4a6cab3eb2428cabb6a8564ac22a67
fortunatozm <fortunatompongo@hotmail.com>
2023-01-23 16:52:44 -0300

    Pagina register front-end

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter, Route } from 'react-router-dom';
 import rockGlass from './images/rockGlass.svg';
+import Registers from './components/Register';
 
 function App() {
   return (
-    <div className="App">
-      <span className="logo">TRYBE</span>
-      <object className="rocksGlass" type="image/svg+xml" data={ rockGlass }>
-        Glass
-      </object>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <span className="logo">TRYBE</span>
+        <object className="rocksGlass" type="image/svg+xml" data={ rockGlass }>
+          Glass
+        </object>
+      </div>
+      <Route exact path="/register" component={ Registers } />
+    </BrowserRouter>
   );
 }
 
