4ef07c35363e6072dd18327946d6119e599a9f18
Lauro Pereira <lauropera01@gmail.com>
2023-01-24 10:34:29 -0300

    remove old app.js

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import Login from './pages/Login';
+import Registers from './components/Register';
 
 function App() {
   return (
     <Switch>
       <Redirect exact from="/" to="/login" />
       <Route path="/login" component={ Login } />
+      <Route exact path="/register" component={ Registers } />
     </Switch>
   );
 }
