798d0bed271a8ed46eec973345dcc8157140b517
Lauro Pereira <lauropera01@gmail.com>
2023-01-24 11:22:26 -0300

    feat: product card

--- front-end/src/pages/Products.jsx
+++ front-end/src/pages/Products.jsx
@@ -1,7 +1,28 @@
 import Navbar from '../components/Navbar';
+import useApiGet from '../hooks/useApiGet';
+import Product from '../components/Product';
 
 function Products() {
-  return <Navbar />;
+  const { data, isFetching } = useApiGet('products');
+
+  return (
+    <div>
+      <Navbar />
+      {!isFetching && (
+        <section>
+          {data.map(({ id, imageUrl, name, price }) => (
+            <Product
+              key={ id }
+              id={ id }
+              imageUrl={ imageUrl }
+              name={ name }
+              price={ price }
+            />
+          ))}
+        </section>
+      )}
+    </div>
+  );
 }
 
 export default Products;
