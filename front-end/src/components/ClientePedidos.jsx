753115e7e80c1726e48700452d1456321598f358
samuel5g <samuelmonteiro21@gmail.com>
2023-01-31 20:44:48 -0300

    refactor(pages): :recycle: Changes some logics to get orders. Not passing in test yet

--- front-end/src/components/ClientePedidos.jsx
+++ front-end/src/components/ClientePedidos.jsx
@@ -1,42 +1,41 @@
 import React, { useEffect, useState } from 'react';
+import useApiGet from '../hooks/useApiGet';
+import Navbar from './Navbar';
 // import { salesByUserId } from '../services/otherService';
-import requests from '../services/requests';
+// import requests from '../services/requests';
 
 function ClienteMeusPedidos() {
+  const { data, isFetching } = useApiGet('pedidos');
   const [pedidos, setPedidos] = useState([]);
-  useEffect(() => {
-    const dadosSales = async () => {
-      const data = await requests.get.pedidos();
-      console.log(data);
-      setPedidos(data);
-    };
-    dadosSales();
-  }, []);
 
-  if (pedidos.length > 0) {
-    pedidos.map((pedido) => (
-      <div key={ pedido.id }>
-        chegou aqui
-        <span data-testid={ `customer_orders__element-order-id-${pedido.id}` }>
-          Pedido
-        </span>
-        <span data-testid={ `customer_orders__element-delivery-status-${pedido.id}` }>
-          Status
-        </span>
-        <span data-testid={ `customer_products__element-order-date-${pedido.id}` }>
-          Data
-        </span>
-        <span data-testid={ `customer_orders__element-card-price-${pedido.id}` }>
-          Preço
-        </span>
-      </div>
-    ));
-  }
+  useEffect(() => {
+    if (!isFetching) {
+      setPedidos(data);
+    }
+  }, [data, isFetching]);
+
   return (
     <div>
-      <p>
-        O cliente não fez nenhum pedido
-      </p>
+      <Navbar />
+
+      {pedidos.map(({ id, status, saleDate, totalPrice }) => (
+        <div key={ id }>
+          <span data-testid={ `customer_orders__element-order-id-${id}` }>
+            Pedido
+            {' '}
+            {id}
+          </span>
+          <span data-testid={ `customer_orders__element-delivery-status-${id}` }>
+            {status}
+          </span>
+          <span data-testid={ `customer_orders__element-order-date-${id}` }>
+            {saleDate}
+          </span>
+          <span data-testid={ `customer_orders__element-card-price-${id}` }>
+            {totalPrice}
+          </span>
+        </div>
+      ))}
     </div>
   );
 }
